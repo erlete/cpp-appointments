9802025caab89554ac2c000da95f871c0002f1e9
Paulo Sanchez <szblzpaulo@gmail.com>
2021-09-08 14:32:44 +0200

    Minor naming modifications

--- 02.Arrays.cpp
+++ 02.Arrays.cpp
@@ -1,19 +1,24 @@
 #include <iostream>
-
-using namespace std;
+#include <string>
+#include <array>
 
 int main() {
+    std::cout << "Arrays:";
     // Array definition (with size):
     int array[2] = {1, 2};
-    cout << array[0] << array[1];
+    std::cout << "\nElements:";
+    for (int i = 0; i < sizeof(array); i++) {
+        std::cout << "\n\tElement " << i << ": " << array[i];
+    }
     
     // Element redefinition:
     array[0] = 3;
-    cout << array[0] << array[1];
+    std::cout << array[0] << array[1];
     
     // Array definition (without size):
     int array2[] = {1, 2, 3, 4};
     // If more elements have to be added to the array, it will have to be completely redefined.
-    cout << array2;
+    std::cout << array2;
+    
     return 0;
 }
